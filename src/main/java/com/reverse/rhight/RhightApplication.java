f94fce12e413c918ab9416ff2626f25b21d14fa4
Gyuwon-Jung <licmm0520@gmail.com>
2026-02-23 11:02:55 +0900

    initialize project

---
+++ src/main/java/com/reverse/rhight/RhightApplication.java
@@ -0,0 +1,13 @@
+package com.reverse.rhight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reverse/rhight/RhightApplicationTests.java
@@ -0,0 +1,19 @@
+package com.reverse.rhight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+
+@SpringBootTest
+class RhightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+    @Test
+    void verifyModularArchitecture() {
+        // 이 테스트가 실행될 때, 모듈 간의 규칙 위반이 없는지 전체를 스캔합니다.
+        ApplicationModules.of(RhightApplication.class).verify();
+    }
+}
